6ece49ed753a6abc74c148601c1e4910406a98ae
Austin <pizzahi5@gmail.com>
2022-10-25 17:21:48 -0700

    Solidity_version_0.6.12

--- src/DiscretionaryCoverage.sol
+++ src/DiscretionaryCoverage.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@enzyme/persistent/global-config/GlobalConfigLib.sol"; //Enzyme is written in verison 0.6.12
+
 contract DiscretionaryCoverage {
-    
+
 }
